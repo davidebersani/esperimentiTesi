5d348cb6306101282db085d6493f8251ab1e8012
Davide Bersani <davide.bersani96@gmail.com>
2020-03-07 19:23:57 +0100

    Inizializzata struttura del progetto. Inizializzato il microservizio printer.

---
+++ sources/printer-service/src/test/java/davide/bersani/printerservice/PrinterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.bersani.printerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
