e731e78203938b5e52a4a49377378119f0656509
Davide Bersani <davide.bersani96@gmail.com>
2020-03-07 17:35:22 +0100

    Aggiunto Vagrantfile per ambiente di test. Aggiunto script per installazine gradle.

---
+++ provisioning/common.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HOME=/home/vagrant 
+DOWNLOADS=${HOME}
+
+
+function downloadExists {
+	FILE=${ASW_DOWNLOADS}/$1
+	if [ -e $FILE ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function fileExists {
+	FILE=$1
+	if [ -e $FILE ]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+#echo "common loaded"
