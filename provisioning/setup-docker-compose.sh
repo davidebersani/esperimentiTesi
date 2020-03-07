3a2555a17dec0f1928352837fef8785b71dbd16f
Davide Bersani <davide.bersani96@gmail.com>
2020-03-07 18:17:25 +0100

    Aggiunto script per installare Docker Compose

---
+++ provisioning/setup-docker-compose.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installo le dipendenze
+echo "=================="
+echo "Installazione Docker Compose"
+echo "=================="
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
