37167d94f315d0b9ad110a01ce5e43d2ccec4619
Davide Bersani <davide.bersani96@gmail.com>
2020-03-07 18:07:47 +0100

    Aggiunto script per installazione Docker

---
+++ provisioning/setup-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installo le dipendenze
+echo "=================="
+echo "Installazione dipendenze Docker"
+echo "=================="
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+echo "=================="
+echo "Installazione Docker"
+echo "=================="
+sudo apt install -y docker-ce
+
