c345fad8041939e4e0cdc48a343975035e9535c9
marco <marco.berbeglia@gmail.com>
2020-04-21 12:41:50 +0200

    aggiunti script per 9 scenari

---
+++ sources/simulations/n_rest_a_call_b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Vengono effettuate N chiamate REST consecutive al servizio A il quale chiama il servizio B
+
+N=50
+echo "Eseguo $N chiamate REST consecutive al servizio A il quale chiama il servizio B"
+
+for (( i=0; i<$N; i++ ))
+do
+    echo "Chiamata $((i+1))"
+    ./curl-client.sh http://localhost:8080/a/prosegui " call B {};"
+    echo ""
+done
+
+echo "Fatto"
---
+++ sources/simulations/n_rest_a_call_b_call_c_sleep2000.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Vengono effettuate N chiamate REST consecutive al servizio A il quale chiama prima il servizio B e poi chiama il servizio C
+
+N=50
+echo "Eseguo $N chiamate REST consecutive al servizio A il quale chiama prima il servizio B e poi chiama il servizio C"
+
+for (( i=0; i<$N; i++ ))
+do
+    echo "Chiamata $((i+1))"
+    ./curl-client.sh http://localhost:8080/a/prosegui " call B { call C {sleep 2000;} ; };"
+    echo ""
+done
+
+echo "Fatto"
