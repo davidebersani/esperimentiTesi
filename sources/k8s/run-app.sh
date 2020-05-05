331a48dbf7bd53a829be5f86112afc2f0150c62b
Davide Bersani <davide.bersani96@gmail.com>
2020-05-05 16:56:17 +0200

    Aggiunte configurazioni per Kafka e servizio A
    - Aggiunte le configurazioni per eseguire Kafka e il servizio A su Kubernetes. 
    - Aggiunti script per avviare e stoppare tutta l'applicazione
    - Modificate proprietà del template service: indirizzo di kafka, proprietà di eureka e zipkin commentante (per il momento), porta fissa sulla 8080
    - Commentate dipendenze di eureka e zipkin (per il momento)
    N.B. L'operazione call non funziona, perchè usa ancora eureka per scoprire gli atri servizi.

---
+++ sources/k8s/run-app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Avvio namespace
+kubectl apply -f template-ns.yml 
+
+# Avvio kafka
+kubectl apply -f zookeeper.yml
+kubectl apply -f kafka-svc.yml
+# echo "==> N.B. Avviare manualmente kafka-deploy.yml"    # Trovare un modo per farlo automaticamente.
+# Ottengo ip del kafka service e lo sostituisco nel kafka deploy
+ip="$(kubectl get service/kafka-svc -n template-ns -o jsonpath='{.spec.clusterIP}')"
+sed "s/<IPFromService>/$ip/g" kafka-deploy.yml > kafka-deploy-modified.yml
+kubectl apply -f kafka-deploy-modified.yml
+rm kafka-deploy-modified.yml
+
+# Avvio servizi
+kubectl apply -f template-services.yml
+
+
