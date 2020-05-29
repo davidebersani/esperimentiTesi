3c4bfd7efb9c3910de9b4c5e602ecf8d65438377
Davide Bersani <davide.bersani96@gmail.com>
2020-05-29 12:22:15 +0200

    Aggiornati script e configurazioni
    - Aggiornato script per build e rilascio, ora è parametrico
    - Aggiornato script per stoppare l'intera applicazione
    - Aggiunta proprietà per influxdb per attivare le query in Flux

---
+++ sources/build-push-rollout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script per buildare un servizio passato come parametro
+if [ "$1" = "help" -o "$#" -ne 2 ]
+then
+  echo "Utilizzo: ./build-publich-rollout.sh <directory> <nome immagine Docker>."
+else
+  cd "$1"
+  echo "==> Avvio build del servizio template"
+  gradle build && \
+
+  echo "==> Avvio build dell'immagine Docker" && \
+  docker build . -t esperimentesi/"$2" && \
+
+  echo "==> Carico l'immagine Docker su DockerHub" && \
+  docker push esperimentesi/"$2" && \
+
+  echo "==> Avvio il rollout"
+  # Si presuppone che i deployment si chiamino <nome immagine>-deployment
+  kubectl rollout restart deployment/"$2"-deployment -n template-ns && \
+  echo "==> Rollout avviato, controlla il completamento"
+fi
--- sources/stop-k8s-app.sh
+++ sources/stop-k8s-app.sh
@@ -5,4 +5,6 @@
 # kubectl delete -f kafka-svc.yml
 # kubectl delete -f zookeeper-deploy.yml
 
+kubectl get pv | tail -n+2 | awk '{print $1}' | xargs -I{} kubectl patch pv {} --type='merge' -p '{"metadata":{"finalizers": null}}'
 kubectl delete -f template-ns.yml
+kubectl delete pv data2
