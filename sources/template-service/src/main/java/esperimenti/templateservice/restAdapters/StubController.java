9c10c491f82ac4ce0bbf5ed38f038c854b849544
Davide Bersani <davide.bersani96@gmail.com>
2020-05-31 15:59:45 +0200

    Fix typo

--- sources/template-service/src/main/java/esperimenti/templateservice/restAdapters/StubController.java
+++ sources/template-service/src/main/java/esperimenti/templateservice/restAdapters/StubController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudController {
+public class StubController {
 
     @Autowired
     MeterRegistry meterRegistry;
